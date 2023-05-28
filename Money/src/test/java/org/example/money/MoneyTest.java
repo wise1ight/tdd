abdebcc5a28009f5ada103c25c763c4aaf3c1892
김현욱 <company@wiselight.kr>
2023-05-28 18:59:06 +0900

    test: Dollar 객체간 비교로 변경

--- Money/src/test/java/org/example/money/MoneyTest.java
+++ Money/src/test/java/org/example/money/MoneyTest.java
@@ -9,9 +9,9 @@ public class MoneyTest {
     public void testMultiplication() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        Assertions.assertEquals(10, product.amount);
+        Assertions.assertEquals(new Dollar(10), product);
         product = five.times(3);
-        Assertions.assertEquals(15, product.amount);
+        Assertions.assertEquals(new Dollar(15), product);
     }
 
     @Test
