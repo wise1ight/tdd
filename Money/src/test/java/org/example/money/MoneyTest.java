f007e74d3b9c8961f7a1a9e5bcee1df5b66d4ede
김현욱 <company@wiselight.kr>
2023-05-28 18:28:18 +0900

    chore: Test 클래스 이름 변경

--- Money/src/test/java/org/example/money/MoneyTest.java
+++ Money/src/test/java/org/example/money/MoneyTest.java
@@ -3,7 +3,7 @@ package org.example.money;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class MyFirstTest {
+public class MoneyTest {
 
     @Test
     public void testMultiplication() {
