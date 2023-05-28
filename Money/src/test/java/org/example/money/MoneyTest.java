97fcb104be5f15b08904f6fcaceef0c26bcfb138
김현욱 <company@wiselight.kr>
2023-05-28 18:44:31 +0900

    test: 새로운 객체를 반환하는 테스트

--- Money/src/test/java/org/example/money/MoneyTest.java
+++ Money/src/test/java/org/example/money/MoneyTest.java
@@ -8,9 +8,9 @@ public class MoneyTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        Assertions.assertEquals(10, five.amount);
-        five.times(3);
-        Assertions.assertEquals(15, five.amount);
+        Dollar product = five.times(2);
+        Assertions.assertEquals(10, product.amount);
+        product = five.times(3);
+        Assertions.assertEquals(15, product.amount);
     }
 }
