c28faa0f9bb5c7d02d623f8bed7ee47e716755b8
김현욱 <company@wiselight.kr>
2023-06-11 18:45:24 +0900

    feat: 부모 클래스에서 times 메서드 지원

--- Money/src/main/java/org/example/money/Money.java
+++ Money/src/main/java/org/example/money/Money.java
@@ -1,6 +1,6 @@
 package org.example.money;
 
-public class Money {
+public abstract class Money {
 
     protected int amount;
 
@@ -13,4 +13,6 @@ public class Money {
     static Dollar dollar(int amount) {
         return new Dollar(amount);
     }
+
+    abstract Money times(int multiplier);
 }
