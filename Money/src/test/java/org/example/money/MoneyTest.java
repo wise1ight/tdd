5c26367fb0beb2695aa859bf50927d31783f500f
김현욱 <company@wiselight.kr>
2023-06-11 19:26:04 +0900

    test: Expression 테스트 코드 추가

--- Money/src/test/java/org/example/money/MoneyTest.java
+++ Money/src/test/java/org/example/money/MoneyTest.java
@@ -29,6 +29,9 @@ public class MoneyTest {
     public void testSimpleAddition() {
         Money sum = Money.dollar(5).plus(Money.dollar(5));
         Assertions.assertEquals(Money.dollar(10), sum);
+
+        Money reduced = bank.reduce(sum, "USD");
+        Assertions.assertEquals(Money.dollar(10), reduced);
     }
 
 }
