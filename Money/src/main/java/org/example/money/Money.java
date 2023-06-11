97449eb81886544df3275699fd82325941118abf
김현욱 <company@wiselight.kr>
2023-06-11 18:32:18 +0900

    refactor: equals를 부모 클래스로 이동

--- Money/src/main/java/org/example/money/Money.java
+++ Money/src/main/java/org/example/money/Money.java
@@ -3,4 +3,9 @@ package org.example.money;
 public class Money {
 
     protected int amount;
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount;
+    }
 }
