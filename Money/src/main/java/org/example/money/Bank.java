da982f500793fd6d3d23277c68589379dc7e1772
김현욱 <company@wiselight.kr>
2023-06-11 20:24:55 +0900

    feat: Money일 경우 Money 반환

--- Money/src/main/java/org/example/money/Bank.java
+++ Money/src/main/java/org/example/money/Bank.java
@@ -3,6 +3,7 @@ package org.example.money;
 public class Bank {
 
     Money reduce(Expression source, String to) {
+        if (source instanceof Money) return (Money) source;
         Sum sum = (Sum) source;
         return sum.reduce(to);
     }
