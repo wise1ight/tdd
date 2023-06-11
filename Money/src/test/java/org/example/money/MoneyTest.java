b1bf091cb234f2009172f38fda3c336bd4d7cc85
김현욱 <company@wiselight.kr>
2023-06-11 19:20:36 +0900

    test: plus 메서드에 대한 테스트 추가

--- Money/src/test/java/org/example/money/MoneyTest.java
+++ Money/src/test/java/org/example/money/MoneyTest.java
@@ -25,4 +25,10 @@ public class MoneyTest {
         Assertions.assertEquals("CHF", Money.franc(1).currency());
     }
 
+    @Test
+    public void testSimpleAddition() {
+        Money sum = Money.dollar(5).plus(Money.dollar(5));
+        Assertions.assertEquals(Money.dollar(10), sum);
+    }
+
 }
