5e1efcedc7037b4272e907b8d886e5e4ea69759e
김현욱 <company@wiselight.kr>
2023-06-11 20:17:25 +0900

    feat: 두 Money 합

--- Money/src/main/java/org/example/money/Bank.java
+++ Money/src/main/java/org/example/money/Bank.java
@@ -3,6 +3,8 @@ package org.example.money;
 public class Bank {
 
     Money reduce(Expression source, String to) {
-        return Money.dollar(10);
+        Sum sum = (Sum) source;
+        int amount = sum.augend.amount + sum.addend.amount;
+        return new Money(amount, to);
     }
 }
