c3dde84081a81a950d630548be2e872ee5c808a8
김현욱 <company@wiselight.kr>
2023-06-11 20:56:45 +0900

    test: 혼합 덧셈 테스트

--- Money/src/test/java/org/example/money/MoneyTest.java
+++ Money/src/test/java/org/example/money/MoneyTest.java
@@ -71,4 +71,15 @@ public class MoneyTest {
         Assertions.assertEquals(1, new Bank().rate("USD", "USD"));
     }
 
+    @Test
+    public void testMixedAddition() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(
+                fiveBucks.plus(tenFrancs), "USD");
+        Assertions.assertEquals(Money.dollar(10), result);
+    }
+
 }
