002bb7a5e7998948e933b8b41c3a35214fb4ea74
김현욱 <company@wiselight.kr>
2023-06-11 18:47:48 +0900

    test: Dollar에 대하여 팩토리 메서드 적용

--- Money/src/test/java/org/example/money/MoneyTest.java
+++ Money/src/test/java/org/example/money/MoneyTest.java
@@ -8,17 +8,17 @@ public class MoneyTest {
     @Test
     public void testMultiplication() {
         Money five = Money.dollar(5);
-        Assertions.assertEquals(new Dollar(10), five.times(2));
-        Assertions.assertEquals(new Dollar(15), five.times(3));
+        Assertions.assertEquals(Money.dollar(10), five.times(2));
+        Assertions.assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
     public void testEquality() {
-        Assertions.assertTrue(new Dollar(5).equals(new Dollar(5)));
-        Assertions.assertFalse(new Dollar(5).equals(new Dollar(6)));
+        Assertions.assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        Assertions.assertFalse(Money.dollar(5).equals(Money.dollar(6)));
         Assertions.assertTrue(new Franc(5).equals(new Franc(5)));
         Assertions.assertFalse(new Franc(5).equals(new Franc(6)));
-        Assertions.assertFalse(new Franc(5).equals(new Dollar(5)));
+        Assertions.assertFalse(new Franc(5).equals(Money.dollar(5)));
     }
 
     @Test
