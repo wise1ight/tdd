52d6a13686c19be103a5a38354c14e2fa67d5ce3
김현욱 <company@wiselight.kr>
2023-06-11 20:26:59 +0900

    feat: Expression 인터페이스에 reduce 추가

--- Money/src/main/java/org/example/money/Expression.java
+++ Money/src/main/java/org/example/money/Expression.java
@@ -1,4 +1,5 @@
 package org.example.money;
 
 public interface Expression {
+    Money reduce(String to);
 }
--- Money/src/main/java/org/example/money/Money.java
+++ Money/src/main/java/org/example/money/Money.java
@@ -32,6 +32,10 @@ public class Money implements Expression {
         return new Sum(this, addend);
     }
 
+    public Money reduce(String to) {
+        return this;
+    }
+
     String currency() {
         return this.currency;
     }
