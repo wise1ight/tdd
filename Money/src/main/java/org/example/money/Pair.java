4efd6106b13294d7a5daa861ed25b09115b81693
김현욱 <company@wiselight.kr>
2023-06-11 20:52:01 +0900

    feat: Bank에서 rate 관리

--- Money/src/main/java/org/example/money/Pair.java
+++ Money/src/main/java/org/example/money/Pair.java
@@ -3,7 +3,7 @@ package org.example.money;
 public class Pair {
     private String from;
     private String to;
-    
+
     Pair(String from, String to) {
         this.from = from;
         this.to = to;
