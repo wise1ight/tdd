ab800eff7d00be54a4d92c380f98f3342aa6d4e6
김현욱 <company@wiselight.kr>
2023-06-11 18:58:20 +0900

    feat: 팩토리 메서드에서 CHF 전달

--- Money/src/main/java/org/example/money/Franc.java
+++ Money/src/main/java/org/example/money/Franc.java
@@ -6,7 +6,7 @@ public class Franc extends Money {
 
     Franc(int amount, String currency) {
         this.amount = amount;
-        this.currency = "CHF";
+        this.currency = currency;
     }
 
     Money times(int multiplier) {
