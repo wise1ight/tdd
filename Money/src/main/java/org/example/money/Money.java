d02fe3a301c927ef955b04ef800219d04b13572c
김현욱 <company@wiselight.kr>
2023-06-11 19:25:53 +0900

    feat: plus 메서드 구현

--- Money/src/main/java/org/example/money/Money.java
+++ Money/src/main/java/org/example/money/Money.java
@@ -28,6 +28,10 @@ public class Money {
         return new Money(amount * multiplier, currency);
     }
 
+    Money plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
+    }
+
     String currency() {
         return this.currency;
     }
