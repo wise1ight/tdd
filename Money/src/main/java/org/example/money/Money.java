328300f6ba16b91e59acb3ba47b689c80d5a177d
김현욱 <company@wiselight.kr>
2023-06-11 19:02:20 +0900

    refactor: 생성자 구현을 상위 클래스로 올리기

--- Money/src/main/java/org/example/money/Money.java
+++ Money/src/main/java/org/example/money/Money.java
@@ -5,6 +5,11 @@ public abstract class Money {
     protected int amount;
     protected String currency;
 
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
