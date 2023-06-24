2e52f3950a1250544b0e49ff97234470375ebebe
김현욱 <company@wiselight.kr>
2023-06-25 02:26:34 +0900

    test: testSuite

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -79,6 +79,13 @@ class TestCaseTest(TestCase):
         result.testFailed()
         assert("1 run, 1 failed" == result.summary())
 
+    def testSuite(self):
+        suite = TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        result = suite.run()
+        assert("2 run, 1 failed" == result.summary())
+
 
 print(TestCaseTest("testTemplateMethod").run().summary())
 print(TestCaseTest("testResult").run().summary())
