eacc3cd79231909523f347bf1d8370eb047ab7b8
김현욱 <company@wiselight.kr>
2023-06-25 01:52:35 +0900

    test: tearDown 로그 검사

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -34,7 +34,7 @@ class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run()
-        assert("setUp testMethod " == test.log)
+        assert("setUp testMethod tearDown " == test.log)
 
 
 TestCaseTest("testRunning").run()
