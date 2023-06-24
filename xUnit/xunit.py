320a7fe522de4da7a302f7d9083d1ba53095f9cb
김현욱 <company@wiselight.kr>
2023-06-25 02:25:00 +0900

    test: 중복

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -80,7 +80,7 @@ class TestCaseTest(TestCase):
         assert("1 run, 1 failed" == result.summary())
 
 
-TestCaseTest("testTemplateMethod").run()
-TestCaseTest("testResult").run()
-TestCaseTest("testFailedResult").run()
-TestCaseTest("testFailedResultFormatting").run()
+print(TestCaseTest("testTemplateMethod").run().summary())
+print(TestCaseTest("testResult").run().summary())
+print(TestCaseTest("testFailedResultFormatting").run().summary())
+print(TestCaseTest("testFailedResult").run().summary())
