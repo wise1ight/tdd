8045861fe91f10a5bfd24443fa8b94db86ceefaf
김현욱 <company@wiselight.kr>
2023-06-24 22:47:32 +0900

    test: 테스트 단순화

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -28,7 +28,6 @@ class WasRun(TestCase):
 class TestCaseTest(TestCase):
     def testRunning(self):
         test = WasRun("testMethod")
-        assert(not test.wasRun)
         test.run()
         assert test.wasRun
 
