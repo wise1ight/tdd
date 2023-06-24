0a0e0373c5273709cd394cfad216a8cf73879ceb
김현욱 <company@wiselight.kr>
2023-06-24 21:25:01 +0900

    feat: Expression에 plus() 추가

--- Money/src/main/java/org/example/money/Expression.java
+++ Money/src/main/java/org/example/money/Expression.java
@@ -2,4 +2,5 @@ package org.example.money;
 
 public interface Expression {
     Money reduce(Bank bank, String to);
+    Expression plus(Expression addend);
 }
--- Money/src/main/java/org/example/money/Money.java
+++ Money/src/main/java/org/example/money/Money.java
@@ -28,7 +28,7 @@ public class Money implements Expression {
         return new Money(amount * multiplier, currency);
     }
 
-    Expression plus(Expression addend) {
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
